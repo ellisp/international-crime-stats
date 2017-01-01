e03478324f4684506bb1bd402e4dd915b877c868
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-01-02 12:20:30 +1300

    delete some old intermediate files

--- grooming/download-small-arms-survey-2007.R
+++ grooming/download-small-arms-survey-2007.R
@@ -1,10 +1,8 @@
 library(tidyverse)
 library(purrr)
 library(tabulizer)
-library(ISOcodes)
 
-
-
+# Installing tabulizer is non-trivial, adapt the following depending on where the JDK is:
 # Sys.setenv(JAVA_HOME = "C:/Program Files/Java/jdk1.7.0_79")
 # ghit::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"), INSTALL_opts = "--no-multiarch")
 
@@ -33,13 +31,6 @@ sas_orig <- as.data.frame(do.call("rbind", sas2)) %>%
   spread(variable, value) %>%
   arrange(`Rank by rate of ownership`) 
 
-# This was a one off, then hand-edited, to get a concordance of Small Arms Survey
-# "country" names to ISO codes:
-# data(ISO_3166_1)  
-# missing <- data.frame(Name = sas_orig$Country[!sas_orig$Country %in% ISO_3166_1$Name])
-# ISO3166 <- plyr::rbind.fill(missing, ISO_3166_1)
-# write.csv(ISO3166, "data/conc_iso3166.csv", row.names = FALSE)
-
 ISO3166 <- read_csv("data/conc_iso3166.csv", na = "") # na.strings important otherwise Namibia is an NA!
 
 sas_df2 <- sas_orig  %>%
@@ -58,15 +49,6 @@ sas_df2 <- sas_orig  %>%
   left_join(ISO3166, by = c("Country" = "Name"))
 
 
-ggplot(sas_df2, aes(y = `DerivedAverageFirearms`, x = `OriginalAverageFirearms`, label = Country)) +
-  geom_abline(slope = 1, colour = "white") +
-  geom_text(size = 3) +
-  scale_x_log10(breaks = c(1, 10, 100)) +
-  scale_y_log10(breaks = c(1, 10, 100)) +
-  coord_equal()
-
-
-
 # create a CSV for inspecting by hand to see which ones probably wrong
 # sas_orig %>%
 #   mutate(DerivedAverage = round(`Average total all civilian firearms` / `Population, 2005` * 100, 1),
@@ -79,6 +61,3 @@ ggplot(sas_df2, aes(y = `DerivedAverageFirearms`, x = `OriginalAverageFirearms`,
 #   write.csv("data/population-checks.csv", row.names = FALSE)
 
 
-# sas_orig
-
-save(sas_df2, file = "data/sas_df.rda")
